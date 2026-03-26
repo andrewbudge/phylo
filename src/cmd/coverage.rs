5637da4494ccf39684d0167f6849bb0eb200d6b2
Andrew Budge <andrewcbudge@gmail.com>
2026-03-26 00:56:46 -0600

    made the output a little more informative and corrected math for missing_pct since I have the math skills of a rock

--- src/cmd/coverage.rs
+++ src/cmd/coverage.rs
@@ -17,12 +17,17 @@ pub fn run(args: CoverageArgs) {
         .next()
         .expect("empty file")
         .expect("Failed to read header");
+    println!("taxa \t loci_present \t loci_missing \t pct_missing");
     for line in lines {
         let line = line.expect("Failed to read line");
         let fields: Vec<&str> = line.split('\t').collect();
         let total_loci = header.split('\t').count() - 1;
         let present_count = fields[1..].iter().filter(|f| **f != "MISSING").count();
-        let missing_pct = (present_count as f64 / total_loci as f64) * 100.0;
-        println!("{} \t {}%", fields[0], missing_pct);
+        let missing_count = total_loci as f64 - present_count as f64;
+        let missing_pct = (missing_count as f64 / total_loci as f64) * 100.0;
+        println!(
+            "{} \t {}/{} \t {}/{} \t {}%",
+            fields[0], present_count, total_loci, missing_count, total_loci, missing_pct
+        );
     }
 }
